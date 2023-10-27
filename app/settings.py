6cf806fd2c9b41e2c702776d90dc6a99122f0a4c
Vadim Yaskshigulov <toadharvard@gmail.com>
2023-10-27 17:19:58 +0300

    chore: add settings

---
+++ app/settings.py
@@ -0,0 +1,48 @@
+from functools import cache, cached_property
+
+from dotenv import load_dotenv, find_dotenv
+from pydantic import AmqpDsn, computed_field, PostgresDsn
+from pydantic_settings import BaseSettings
+
+load_dotenv(find_dotenv(".env"))
+
+
+class Settings(BaseSettings):
+    postgres_dialect_driver: str = "postgresql"
+
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_db: str
+    postgres_port: int = 5432
+
+    rabbitmq_default_user: str
+    rabbitmq_default_password: str
+    rabbitmq_host: str
+    rabbitmq_port: int = 5672
+
+    @cached_property
+    @computed_field
+    def rabbitmq_dsn(self) -> AmqpDsn:
+        return AmqpDsn.build(
+            scheme=self.postgres_dialect_driver,
+            username=self.rabbitmq_default_user,
+            password=self.rabbitmq_default_password,
+            host=self.rabbitmq_host,
+            port=self.rabbitmq_port,
+        )
+
+    @cached_property
+    @computed_field
+    def postgres_dsn(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="amqp",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=self.postgres_db,
+        )
+
+
+get_settings = cache(lambda: Settings())
---
+++ app/tasks/dummy_task.py
@@ -0,0 +1,13 @@
+import ctypes
+import random
+import time
+
+from app.worker import taskq
+
+
+@taskq.task(bind=True)
+def dummy_task(self, x):
+    time.sleep(1)
+    if random.random() < 0.5:
+        ctypes.string_at(0)
+    return x + 1, self.request.id
