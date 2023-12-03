2f7a1463ce519f3bdfd2552207ce927da19e4c25
raf-nr <nurmuhametovrafik04@gmail.com>
2023-12-03 23:52:56 +0300

    chore: rename module with background tasks

--- app/worker.py
+++ app/worker.py
@@ -6,6 +6,6 @@ settings = get_settings()
 taskq = Celery(
     __name__,
     broker=settings.rabbitmq_dsn.unicode_string(),
-    include=["app.tasks"],
+    include=["app.background_tasks"],
 )
 taskq.config_from_object("app.settings.celery_config")
