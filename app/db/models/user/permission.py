bef10193c7be311b2e3af97b4a7d8d74141a36ec
raf-nr <nurmuhametovrafik04@gmail.com>
2023-12-22 16:06:47 +0300

    feat: add database models for user information

---
+++ app/db/models/user/permission.py
@@ -0,0 +1,18 @@
+from enum import StrEnum, auto
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db import Base
+
+
+class AllPermissions(StrEnum):
+    CAN_USE_BUILTIN_DATASETS = auto()
+    CAN_USE_OWN_DATASETS = auto()
+    CAN_USE_USERS_DATASETS = auto()
+    CAN_VIEW_ADMIN_INFO = auto()
+    CAN_MANAGE_USERS_SESSIONS = auto()
+    CAN_MANAGE_APP_CONFIG = auto()
+
+
+class Permission(Base):
+    __tablename__ = "Permission"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    permission: Mapped[AllPermissions]
