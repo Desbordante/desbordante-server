9caf41eb7b22f631f9d3934a36c0a112494f0b0d
Vadim Yakshigulov <toadharvard@gmail.com>
2024-03-13 14:27:36 +0300

    feat: include task router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .ping import router as ping_router
+from .task import router as task_router
 
 router = APIRouter(prefix="/api")
 router.include_router(ping_router)
+router.include_router(task_router)
