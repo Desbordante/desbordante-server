2bdaec49bb2faced99203f5f438751b23a2847aa
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-19 01:16:48 +0300

    feat(infra): add dataset model for relational data bases

--- internal/infrastructure/data_storage/relational/model/file/__init__.py
+++ internal/infrastructure/data_storage/relational/model/file/__init__.py
@@ -1,1 +1,2 @@
 from internal.infrastructure.data_storage.relational.model.file.file_metadata import FileMetadataORM
+from internal.infrastructure.data_storage.relational.model.file.dataset import DatasetORM
---
+++ internal/infrastructure/data_storage/relational/model/file/dataset.py
@@ -0,0 +1,28 @@
+import typing
+from uuid import uuid4, UUID
+
+from sqlalchemy import ForeignKey, Integer, ARRAY
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from internal.infrastructure.data_storage.relational.model import ORMBaseModel
+from internal.infrastructure.data_storage.relational.model.file.file_metadata import FileMetadataORM
+
+if typing.TYPE_CHECKING:
+    pass
+
+
+class DatasetORM(ORMBaseModel):
+    __tablename__ = "dataset"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+
+    is_built_in: Mapped[bool] = mapped_column(default=False)
+    header: Mapped[list[int]] = mapped_column(ARRAY(Integer), default=[])
+    separator: Mapped[str]
+    file_id: Mapped[UUID] = mapped_column(ForeignKey("file_metadata.id"), nullable=False)
+    file: Mapped[FileMetadataORM] = relationship("FileMetadataORM")
+
+    # related_tasks: Mapped[list["TaskORM"]] = relationship(
+    #     "TaskORM", back_populates="dataset"
+    # )
+
+    # owner = relationship("UserORM")
