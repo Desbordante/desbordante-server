04d67bc0d095380cad6edbcbc68d6cf6dfb278b6
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-18 16:57:33 +0300

    feat(tests): add tests for save dataset use case

---
+++ tests/usecase/test_save_dataset.py
@@ -0,0 +1,63 @@
+from uuid import uuid4
+
+import pytest
+from pytest_mock import MockerFixture
+
+from internal.dto.repository.file import DatasetResponseSchema, DatasetCreateSchema
+from internal.uow import UnitOfWork, DataStorageContext
+from internal.usecase.file.save_dataset import DatasetRepo, SaveDataset
+
+
+@pytest.fixture
+def unit_of_work_mock(mocker: MockerFixture) -> UnitOfWork:
+    mock = mocker.MagicMock()
+    mock.__enter__.return_value = mocker.Mock(
+        return_value=mocker.Mock(), spec=DataStorageContext
+    )
+    mock.__exit__.return_value = mocker.Mock(return_value=None)
+    return mock
+
+
+@pytest.fixture
+def dataset_repo_mock(mocker: MockerFixture) -> DatasetRepo:
+    mock = mocker.Mock(spec=DatasetRepo)
+    return mock
+
+
+@pytest.fixture
+def save_dataset(
+    unit_of_work_mock: UnitOfWork, dataset_repo_mock: DatasetRepo
+) -> SaveDataset:
+    return SaveDataset(unit_of_work=unit_of_work_mock, dataset_repo=dataset_repo_mock)
+
+
+def test_save_dataset(
+    save_dataset: SaveDataset,
+    unit_of_work_mock: UnitOfWork,
+    dataset_repo_mock: DatasetRepo,
+) -> None:
+    # Prepare data
+    file_id = uuid4()
+    dataset_id = uuid4()
+    separator = "?"
+    header = [1, 2, 3]
+
+    dataset_repo_mock.create.return_value = DatasetResponseSchema(
+        id=dataset_id, file_id=file_id, separator=separator, header=header
+    )
+
+    # Act
+    result_id = save_dataset(file_id=file_id, separator=separator, header=header)
+
+    # Check that the create method was called with the correct arguments
+    dataset_repo_mock.create.assert_called_once_with(
+        DatasetCreateSchema(file_id=file_id, separator=separator, header=header),
+        unit_of_work_mock.__enter__.return_value,
+    )
+
+    # Check that UnitOfWork was entered and exited correctly
+    unit_of_work_mock.__enter__.assert_called_once()
+    unit_of_work_mock.__exit__.assert_called_once()
+
+    # Verify that the result of the use case matches the expected dataset_id
+    assert result_id == dataset_id
