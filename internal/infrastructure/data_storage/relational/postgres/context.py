1ad0cd35d47532e6955ee73cf5fc12e30f777a95
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-19 03:10:44 +0300

    chore(infra): rename and replace relational storage context

--- internal/infrastructure/data_storage/relational/postgres/context.py
+++ internal/infrastructure/data_storage/relational/postgres/context.py
@@ -1,11 +1,10 @@
 from typing import Generator
 
 from sqlalchemy import create_engine, NullPool
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
 from internal.infrastructure.data_storage import settings
-
-DataStorageContext = Session
+from internal.infrastructure.data_storage.relational.context import RelationalStorageContext
 
 default_engine = create_engine(url=settings.postgres_dsn.unicode_string())
 engine_without_pool = create_engine(
@@ -17,16 +16,16 @@ ContextLocal = sessionmaker(bind=default_engine)
 ContextLocalWithoutPool = sessionmaker(bind=engine_without_pool)
 
 
-def get_context() -> Generator[DataStorageContext, None, None]:
+def get_context() -> Generator[RelationalStorageContext, None, None]:
     """
-    Returns a generator that yields a context(session) object for database operations.
+    Returns a generator that yields a context.py(session) object for database operations.
     """
     with ContextLocal() as context:
         yield context
 
-def get_context_without_pool() -> Generator[DataStorageContext, None, None]:
+def get_context_without_pool() -> Generator[RelationalStorageContext, None, None]:
     """
-    Returns a generator that yields a context(session) object without pool for database operations.
+    Returns a generator that yields a context.py(session) object without pool for database operations.
     """
     with ContextLocalWithoutPool() as context:
         yield context
