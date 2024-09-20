efb491360119d833355f132835332070efe19278
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-20 19:09:36 +0300

    feat(domain): add a function to get a primitive by its name

--- internal/domain/task/entities/__init__.py
+++ internal/domain/task/entities/__init__.py
@@ -1,2 +1,14 @@
+from typing import assert_never
+
 from internal.domain.task.entities.fd import FdTask
 from internal.domain.task.entities.afd import AfdTask
+from internal.domain.task.value_objects import PrimitiveName
+
+
+def match_task_by_primitive_name(primitive_name: PrimitiveName):
+    match primitive_name:
+        case PrimitiveName.fd:
+            return FdTask()
+        case PrimitiveName.afd:
+            return AfdTask()
+    assert_never(primitive_name)
