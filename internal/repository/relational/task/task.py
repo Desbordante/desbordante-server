812b5239ce86edfd565bec2ff243f830e6ee84dc
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-21 01:32:25 +0300

    feat(repo): add task repository implementation

---
+++ internal/repository/relational/task/task.py
@@ -0,0 +1,17 @@
+from internal.infrastructure.data_storage.relational.model.task import TaskORM
+from internal.repository.relational import CRUD
+from internal.dto.repository.task import TaskCreateSchema, TaskUpdateSchema, TaskFindSchema, TaskResponseSchema
+
+
+class TaskRepository(
+    CRUD[
+        TaskORM,
+        TaskCreateSchema,
+        TaskUpdateSchema,
+        TaskFindSchema,
+        TaskResponseSchema
+    ]
+):
+
+    def __init__(self):
+        super().__init__(orm_model=TaskORM, response_schema=TaskResponseSchema)
