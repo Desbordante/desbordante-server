5ba0b87f3867779f2e6725f25a033ba4f3ce05ae
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-21 01:51:16 +0300

    feat(infra): add the main celery worker

---
+++ internal/infrastructure/background_task/celery/__init__.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+from internal.infrastructure.data_storage import settings
+
+worker = Celery(
+    __name__,
+    broker=settings.rabbitmq_dsn.unicode_string(),
+    include=['internal.infrastructure.background_task.celery.task'],
+)
+
+worker.config_from_object("internal.infrastructure.background_task.celery.config")
