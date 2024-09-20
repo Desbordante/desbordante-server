a917b95836dd9ef8ad638b087170334203f7f859
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-21 02:05:07 +0300

    feat(rest): add endpoint for upload csv datasets

--- internal/rest/http/file/__init__.py
+++ internal/rest/http/file/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from internal.rest.file.upload_csv_dataset import router as upload_csv_file_router
-from internal.rest.file.retrieve_dataset import router as retrieve_dataset_router
+from internal.rest.http.file.upload_csv_dataset import router as upload_csv_file_router
 
 router = APIRouter(prefix="/file", tags=["file"])
 
 router.include_router(upload_csv_file_router)
-router.include_router(retrieve_dataset_router)
---
+++ internal/rest/http/file/upload_csv_dataset.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import Form, UploadFile, Depends, APIRouter
+
+from internal.rest.http.file.di import get_save_file_use_case, get_save_dataset_use_case, get_check_content_type_use_case
+from internal.usecase.file import SaveFile, SaveDataset, CheckContentType
+
+router = APIRouter()
+
+@router.post("/csv")
+async def upload_csv_dataset(
+    file: UploadFile,
+    separator: Annotated[str, Form()],  # ?separator=","
+    header: Annotated[list[int], Form()],  # ?header=0?header=1?header=2,
+
+    check_content_type: CheckContentType = Depends(get_check_content_type_use_case),
+    save_file: SaveFile = Depends(get_save_file_use_case),
+    save_dataset: SaveDataset = Depends(get_save_dataset_use_case)
+) -> UUID:
+
+    check_content_type(upload_file=file)
+    save_file_result = await save_file(upload_file=file)
+    save_dataset_result = save_dataset(
+        file_id=save_file_result.id,
+        separator=separator,
+        header=header,
+    )
+    return save_dataset_result
