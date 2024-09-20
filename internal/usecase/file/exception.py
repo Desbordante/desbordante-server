c74ca72922ad6bed152f781060cca30b39d70848
raf-nr <nurmuhametovrafik04@gmail.com>
2024-09-20 18:50:09 +0300

    feat(use case): add file metadata not found exception

--- internal/usecase/file/exception.py
+++ internal/usecase/file/exception.py
@@ -1,9 +1,6 @@
 class IncorrectFileFormatException(Exception):
     """
     Exception raised when a file format is incorrect or not supported.
-
-    :param message: The error message to be reported.
-    :type message: str
     """
 
     def __init__(self, message: str):
@@ -14,6 +11,7 @@ class IncorrectFileFormatException(Exception):
             message(str): The error message to be reported.
         """
         super().__init__(message)
+
 
 class DatasetNotFoundException(Exception):
     """
@@ -29,3 +27,20 @@ class DatasetNotFoundException(Exception):
         The default message "Dataset not found" is used.
         """
         super().__init__("Dataset not found")
+
+
+class FileMetadataNotFoundException(Exception):
+    """
+    Exception raised when file metadata is not found.
+
+    This exception is used to indicate that the metadata for a specific file is missing
+    or could not be retrieved, which may impact operations that depend on that metadata.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of FileMetadataNotFoundException with a default message.
+
+        The default message "File metadata not found" is used to indicate the error.
+        """
+        super().__init__("File metadata not found")
