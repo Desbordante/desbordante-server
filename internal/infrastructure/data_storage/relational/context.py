eeff5b9f14a0f8085716bc513131cd1317c44daf
Rafik Nurmuhametov <113212617+raf-nr@users.noreply.github.com>
2024-10-01 15:15:02 +0000

    feat(infra): add relational add/delete models

--- internal/infrastructure/data_storage/relational/context.py
+++ internal/infrastructure/data_storage/relational/context.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
 
 type RelationalContextType = Session
 type RelationalContextMakerType = sessionmaker
+
+RelationalAddModel = DeclarativeBase
+RelationalDeleteModel = DeclarativeBase
