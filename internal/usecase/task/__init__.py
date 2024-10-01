d26bec29124bc0e85094d0e51ab4f05aa790bb30
Rafik Nurmuhametov <113212617+raf-nr@users.noreply.github.com>
2024-10-01 22:33:35 +0000

    chore(usecase): add missed task usecases imports

--- internal/usecase/task/__init__.py
+++ internal/usecase/task/__init__.py
@@ -1,2 +1,4 @@
 from internal.usecase.task.retrieve_task import RetrieveTask  # noqa: F401
 from internal.usecase.task.set_task import SetTask  # noqa: F401
+from internal.usecase.task.profile_task import ProfileTask  # noqa: F401
+from internal.usecase.task.update_task_info import UpdateTaskInfo  # noqa: F401
