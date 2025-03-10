8e3a9292b7bd96d28bfea2ed0e35e17cd2e7c72f
vladyoslav <vaifaer@gmail.com>
2025-03-10 23:12:21 +0300

    feat: settings

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from .auth import router as auth_router
 from .common import router as common_router
 
 router = APIRouter()
 
 router.include_router(common_router, prefix="", tags=["common"])
+router.include_router(auth_router, prefix="/auth", tags=["auth"])
