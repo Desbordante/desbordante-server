28715ee517ca0b958a962eb099bb90d60a034d04
vladyoslav <vaifaer@gmail.com>
2025-04-26 20:43:34 +0300

    feat: logging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import logging
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from src.api import router as api_router
 from src.exceptions import BaseAppException
+from src.logging import configure_logging
+
+configure_logging()
+logger = logging.getLogger(__name__)
 
 app = FastAPI(generate_unique_id_function=lambda route: route.name)
 
@@ -16,10 +22,16 @@ app.add_middleware(
 )
 
 
-# Global exception handler
+# Global exception handlers
 @app.exception_handler(BaseAppException)
 def app_exception_handler(request: Request, exc: BaseAppException):
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+@app.exception_handler(Exception)
+def exception_handler(request: Request, exc: Exception):
+    logger.exception(exc)
+    return JSONResponse(status_code=500, content={"detail": "Internal server error"})
 
 
 app.include_router(api_router)
