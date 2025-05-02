27f57f985b8cdd6f271ff214df201b1661b4877e
pechenux <belokoniy@gmail.com>
2025-05-02 22:40:17 +0300

    feat: impliment sort

--- app/domain/task/schemas/mfd_verification/sort.py
+++ app/domain/task/schemas/mfd_verification/sort.py
@@ -1,25 +1,88 @@
 from enum import StrEnum, auto
-from typing import assert_never
-from .task import HoldsMfdVerificationTaskResult, MfdVerificationModel
+from typing import assert_never, cast, Callable, Union
+from copy import deepcopy
+from .task import (
+    Highlight,
+    HighlightsCluster,
+    HoldsMfdVerificationTaskResult,
+    MfdVerificationModel,
+    NotHoldsMfdVerificationTaskResult,
+)
 from app.domain.task.schemas.base import BaseSorter
 from app.domain.task.schemas.types import SortOrder
 
 
 class MfdVerificationSortOptions(StrEnum):
-    POINT_INDEX = auto()
-    FURTHEST_POINT_INDEX = auto()
+    DATA_INDEX = auto()
+    FURTHEST_DATA_INDEX = auto()
     MAXIMUM_DISTANCE = auto()
 
 
+def sort_highlights_clusters_but_save_indices_order(
+    highlights_clusters: list[HighlightsCluster],
+    is_reverse: bool,
+    sort_fun: Callable[[Highlight], Union[int, float]],
+) -> list[HighlightsCluster]:
+    new_highlights_clusters: list[HighlightsCluster] = []
+    for cluster in highlights_clusters:
+        sorted_highlights = sorted(
+            deepcopy(cluster["highlights"]),
+            key=lambda highlight: (
+                sort_fun(highlight),
+                highlight["highlight_index"] * (-1 if is_reverse else 1),
+            ),
+            reverse=is_reverse,
+        )
+        new_highlights_clusters.append(
+            HighlightsCluster(
+                cluster_index=cluster["cluster_index"],
+                cluster_name=cluster["cluster_name"],
+                max_distance=cluster["max_distance"],
+                highlights_count=len(sorted_highlights),
+                highlights=sorted_highlights,
+            )
+        )
+    return NotHoldsMfdVerificationTaskResult(
+        mfd_holds=False,
+        cluster_count=len(new_highlights_clusters),
+        highlights_clusters=new_highlights_clusters,
+    )
+
+
+def sort_by_point_index(
+    raw_result: NotHoldsMfdVerificationTaskResult, is_reverse: bool
+) -> NotHoldsMfdVerificationTaskResult:
+    return sort_highlights_clusters_but_save_indices_order(
+        cast(list[HighlightsCluster], raw_result["highlights_clusters"]),
+        is_reverse,
+        lambda highlight: highlight["data_index"],
+    )
+
+
+def sort_by_furthest_point_index(
+    raw_result: NotHoldsMfdVerificationTaskResult, is_reverse: bool
+) -> NotHoldsMfdVerificationTaskResult:
+    return sort_highlights_clusters_but_save_indices_order(
+        cast(list[HighlightsCluster], raw_result["highlights_clusters"]),
+        is_reverse,
+        lambda highlight: highlight["furthest_data_index"],
+    )
+
+
 def sort_by_max_distance(
-    raw_result: MfdVerificationModel, is_reverse: bool
-) -> MfdVerificationModel:
-    # raw_result.sort(key=json.dumps, reverse=is_reverse)
-    return HoldsMfdVerificationTaskResult(mfd_holds=True)
+    raw_result: NotHoldsMfdVerificationTaskResult, is_reverse: bool
+) -> NotHoldsMfdVerificationTaskResult:
+    return sort_highlights_clusters_but_save_indices_order(
+        cast(list[HighlightsCluster], raw_result["highlights_clusters"]),
+        is_reverse,
+        lambda highlight: highlight["max_distance"],
+    )
 
 
 class MfdVerificationSorter(BaseSorter):
     _sorter_map = {
+        MfdVerificationSortOptions.DATA_INDEX: sort_by_point_index,
+        MfdVerificationSortOptions.FURTHEST_DATA_INDEX: sort_by_furthest_point_index,
         MfdVerificationSortOptions.MAXIMUM_DISTANCE: sort_by_max_distance,
     }
 
@@ -34,8 +97,15 @@ class MfdVerificationSorter(BaseSorter):
         sort_option: MfdVerificationSortOptions,
         sort_direction: SortOrder,
     ) -> MfdVerificationModel:
+        if raw_result["mfd_holds"]:
+            return cast(
+                HoldsMfdVerificationTaskResult,
+                raw_result,
+            )
+
         is_reverse = sort_direction == SortOrder.DESC
         sorter = self.match_sorter_by_option_name(sort_option)
-        print(raw_result)
-        sorted_result = sorter(raw_result, is_reverse=is_reverse)
+        sorted_result = sorter(
+            cast(NotHoldsMfdVerificationTaskResult, raw_result), is_reverse=is_reverse
+        )
         return sorted_result
