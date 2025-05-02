cd933530fe5c7e5bac9dfb2c4febe0bdcd2af5ef
pechenux <belokoniy@gmail.com>
2025-05-03 00:12:47 +0300

    feat: impliment filter

--- app/domain/task/schemas/mfd_verification/filter.py
+++ app/domain/task/schemas/mfd_verification/filter.py
@@ -1,28 +1,36 @@
+from copy import deepcopy
 from enum import StrEnum, auto
-from typing import List, assert_never
+from typing import assert_never, cast
 from app.domain.task.schemas.base import BaseFilter
-from app.domain.task.schemas.mfd_verification.task import MfdVerificationModel
+from app.domain.task.schemas.mfd_verification.task import (
+    HoldsMfdVerificationTaskResult,
+    MfdVerificationModel,
+    NotHoldsMfdVerificationTaskResult,
+)
 
 
 class MfdVerificationFilterOptions(StrEnum):
-    ATTRIBUTE_NAME = auto()
+    CLUSTER_INDEX = auto()
 
 
-def filter_by_attributes(
-    raw_result: MfdVerificationModel, attributes_names: List[str]
-) -> MfdVerificationModel:
-    return [
-        model
-        for model in raw_result
-        if set(attributes_names).issubset(
-            {sideItem for sideItem in model["lhs"] + model["rhs"]}
-        )
-    ]
+def filter_by_cluster_index(
+    raw_result: NotHoldsMfdVerificationTaskResult, cluster_index: int
+) -> NotHoldsMfdVerificationTaskResult:
+    if (cluster_index < 0) or (cluster_index >= raw_result["cluster_count"]):
+        raise ValueError(f"Invalid cluster index: {cluster_index}")
+
+    return NotHoldsMfdVerificationTaskResult(
+        mfd_holds=False,
+        cluster_count=raw_result["cluster_count"],
+        highlights_clusters=[
+            deepcopy(raw_result["highlights_clusters"][cluster_index])
+        ],
+    )
 
 
 class MfdVerificationFilter(BaseFilter):
     _filter_map = {
-        MfdVerificationFilterOptions.ATTRIBUTE_NAME: filter_by_attributes,
+        MfdVerificationFilterOptions.CLUSTER_INDEX: filter_by_cluster_index,
     }
 
     def match_filter_by_option_name(self, option_name):
@@ -34,8 +42,16 @@ class MfdVerificationFilter(BaseFilter):
         self,
         raw_result: MfdVerificationModel,
         filter_option: MfdVerificationFilterOptions,
-        filter_params: List[str],
+        filter_params: int,
     ) -> MfdVerificationModel:
+        if raw_result["mfd_holds"]:
+            return cast(
+                HoldsMfdVerificationTaskResult,
+                raw_result,
+            )
+
         filter = self.match_filter_by_option_name(filter_option)
-        filtering_result = filter(raw_result, filter_params)
+        filtering_result = filter(
+            cast(NotHoldsMfdVerificationTaskResult, raw_result), filter_params
+        )
         return filtering_result
