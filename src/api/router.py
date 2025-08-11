323cbc2bf4e81758d0602cdf45d36b3983b01326
vladyoslav <vaifaer@gmail.com>
2025-08-11 17:42:35 +0300

    chore(api): v1 prefix

--- src/api/router.py
+++ src/api/router.py
@@ -5,7 +5,7 @@ from src.api.auth import router as auth_router
 from src.api.common import router as common_router
 from src.api.dataset import router as dataset_router
 
-router = APIRouter()
+router = APIRouter(prefix="/v1")
 
 router.include_router(common_router, tags=["common"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
