ead72599536158d75bf6027a62ea09b6db7dd7e4
vladyoslav <vaifaer@gmail.com>
2025-09-27 19:15:25 +0300

    chore(models): user & dataset

--- src/models/dataset_models.py
+++ src/models/dataset_models.py
@@ -1,9 +1,11 @@
-from sqlalchemy import JSON, ForeignKey
+from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.annotations import str_non_nullable, uuid_pk
 from src.models.base_models import BaseModel
 from src.models.user_models import UserModel
+from src.schemas.base_schemas import TaskErrorSchema
 from src.schemas.dataset_schemas import (
     DatasetType,
     OneOfDatasetInfo,
@@ -18,10 +20,12 @@ class DatasetModel(BaseModel):
     name: Mapped[str_non_nullable]
     size: Mapped[int]
     path: Mapped[str_non_nullable]
-    params: Mapped[OneOfDatasetParams] = mapped_column(JSON)
+    params: Mapped[OneOfDatasetParams] = mapped_column(JSONB)
 
-    status: Mapped[TaskStatus] = mapped_column(default=TaskStatus.Pending)
-    info: Mapped[OneOfDatasetInfo | None] = mapped_column(JSON, default=None)
+    status: Mapped[TaskStatus] = mapped_column(default=TaskStatus.PENDING)
+    info: Mapped[OneOfDatasetInfo | TaskErrorSchema | None] = mapped_column(
+        JSONB, default=None
+    )
 
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    owner: Mapped["UserModel"] = relationship(back_populates="datasets", lazy="joined")
+    owner: Mapped["UserModel"] = relationship(back_populates="datasets")
