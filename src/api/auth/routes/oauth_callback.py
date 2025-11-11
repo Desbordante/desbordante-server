8f0245f5257253b571abb9ffa892a163eb10fb12
vladyoslav <vaifaer@gmail.com>
2025-11-12 00:57:38 +0300

    fix(oauth): user adapter

--- src/api/auth/routes/oauth_callback.py
+++ src/api/auth/routes/oauth_callback.py
@@ -7,10 +7,18 @@ from src.api.auth.dependencies import (
     GetOAuthUserInfoUseCaseDep,
     GetOrCreateUserViaOAuthUseCaseDep,
 )
+from src.models.user_models import UserModel
 from src.schemas.auth_schemas import OAuthCredsSchema, OAuthProvider
 from src.schemas.user_schemas import UserSchema
 
 router = APIRouter()
+
+
+class UserAdapter:
+    def __init__(self, user: UserModel):
+        self.id = user.id
+        self.is_admin = user.is_admin
+        self.is_active = user.is_active
 
 
 @router.get(
@@ -28,7 +36,11 @@ async def oauth_callback(
     provider: OAuthProvider = Path(..., description="OAuth provider name"),
 ) -> Any:
     oauth_user_info = await get_oauth_user_info(provider=provider, request=request)
+
     creds = OAuthCredsSchema(provider=provider, oauth_id=oauth_user_info.id)
+
     user = await get_or_create_user_via_oauth(creds=creds)
-    await create_session(request=request, user=user)
+
+    await create_session(request=request, user=UserAdapter(user=user))
+
     return user
