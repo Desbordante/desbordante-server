54c4a2cbbb36535c2ad7284c4934fd3d0c86c787
vladyoslav <vaifaer@gmail.com>
2026-03-01 18:05:42 +0300

    feat: upload dataset use case tests

---
+++ tests/unit/usecases/dataset/constants.py
@@ -0,0 +1,11 @@
+FAKE_USER_ID = 42
+FAKE_FILE_NAME = "dataset.csv"
+FAKE_FILE_SIZE = 1024
+STORAGE_LIMIT = 10 * 1024 * 1024  # 10 MB
+PAYLOAD_TOO_LARGE_MESSAGE = "File size exceeds the maximum allowed size."
+STORAGE_LIMIT_REACHED_MESSAGE = (
+    "Storage limit reached. Delete some datasets to upload a new one."
+)
+FORBIDDEN_CREATE_DATASET_MESSAGE = (
+    "You are not allowed to create this type of datasets."
+)
