b706c59406abe120e6478eb2a2777a5bd05a1264
vladyoslav <vaifaer@gmail.com>
2026-03-18 04:13:11 +0300

    chore: remove outdated DatabaseTaskBase

--- src/domain/task/tasks.py
+++ src/domain/task/tasks.py
@@ -1,14 +1,7 @@
-from typing import Any
-from uuid import UUID
-
 from pydantic import TypeAdapter
 
-from src.crud.task_crud import TaskCrud
 from src.domain.task.utils import download_dataset, get_primitive_class_by_name
 from src.infrastructure.task.resource_intensive_task import ResourceIntensiveTask
-from src.models.task_models import TaskModel
-from src.models.task_result_models import TaskResultModel
-from src.schemas.base_schemas import TaskStatus
 from src.schemas.dataset_schemas import DatasetForTaskSchema
 from src.schemas.task_schemas.base_schemas import (
     OneOfTaskParams,
@@ -18,38 +11,7 @@ from src.schemas.task_schemas.base_schemas import (
 from src.schemas.task_schemas.primitives.base_schemas import PrimitiveResultSchema
 from src.worker.config import settings
 from src.worker.profiling_task_backend import ProfilingTaskBackend
-from src.worker.task import DatabaseTaskBase
 from src.worker.worker import worker
-
-
-class ProfileTaskTask(DatabaseTaskBase[TaskModel, UUID]):
-    crud_class = TaskCrud
-    result_field = "results"
-    error_field = "result"
-
-    def on_success(
-        self,
-        retval: PrimitiveResultSchema[OneOfTaskResultSchema, OneOfTaskResultItemSchema],
-        task_id: str,
-        args: tuple[Any, ...],
-        kwargs: dict[str, Any],
-    ) -> None:
-        id = args[0]
-
-        self._update_object_sync(
-            id,
-            **{
-                self.status_field: TaskStatus.SUCCESS,
-                self.result_field: [
-                    TaskResultModel(
-                        task_id=id,
-                        result=result,
-                    )
-                    for result in retval.items
-                ],
-                "result": retval.result,
-            },
-        )
 
 
 @worker.task(
